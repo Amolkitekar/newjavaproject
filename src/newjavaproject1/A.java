9f60906e100c1768b880525959055083e2231d99
amolkitekar76 <amolkitekar76@gmail.com>
2021-04-02 10:35:21 +0530

    "Change At A"

--- src/newjavaproject1/A.java
+++ src/newjavaproject1/A.java
@@ -3,8 +3,7 @@ package newjavaproject1;
 public class A {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+       System.out.println("Inside A by d1(Modified)...!");
 	}
 
 }
